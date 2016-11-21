a5f729954a1d62420977e6a8aa3515e4fae1108b
Andrea Politi <andrea.politi@statravel.com>
2016-11-21 09:56:04 +0100

    changed solo.rb

--- solo.rb
+++ solo.rb
@@ -1,4 +1,6 @@
-#  file_cache_path "/home/andrea/chef"
-  cookbook_path "/var/chef/cookbooks"
-  json_attribs "/home/andrew/.chef/node.json"
+root = File.absolute_path(File.dirname(__FILE__))
+
+chef_repo_path "/home/andrew/project" 
+cookbook_path "#{chef_repo_path}/cookbooks"
+  json_attribs "#{chef_repo_path}/node.json"
   source 'https://supermarket.chef.io'
