55e5223c2911744cec6a70f5e5de71aa688db81c
Andrea Politi <andrea.politi@statravel.com>
2016-11-21 13:41:28 +0100

    updated readme, modified query results

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # simpleapp
-Project to create a simple app using python2.7 + uwsgi + flask and nginx inside a docker container, all deployed via chef-solo
+Project to create a simple webapp which will extract data from a mysql db using python2.7 + uwsgi + flask and nginx inside a docker container based on ubuntu 16.04, importing a dataset from an external repo, everything deployed via chef-solo
+
+INSTALLATION
+
+Clone the repo:
+
+git clone https://github.com/Andrea-Politi/simpleapp
+
+Install Chef binaries:
+
+$ sudo true && curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
+Then edit the 'chef_repo_path' inside the simpleapp/solo.rb with the app path inside your newly created repo (default is: /home/user/git/simpleapp ) and copy it to /etc/chef/solo.rb. Remember to take a backup if already existing:
+
+& vim simpleapp/solo.rb
+
+Save, then:
+
+$ sudo cp simpleapp/solo.rb /etc/chef/
+
+Clone the external cookbooks:
+
+$ cd simpleapp/cookbooks && git clone https://github.com/chef-cookbooks/mysql
+$ git clone https://github.com/chef-cookbooks/docker
+
+Launch Chef solo:
+
+$ sudo chef-solo
+
+After is finished, open the browser on your host to:
+
+http://<host_IP>:8000
+
+That's it!
--- dkr/sampleapp/app.py
+++ dkr/sampleapp/app.py
@@ -1,57 +1,29 @@
 #!/usr/bin/python
-# view_rows.py - Fetch and display the rows from a MySQL database query
-# import the MySQLdb and sys modules
+# import modules
 import MySQLdb
 import sys
-#import uwsgi
 from flask import Flask
 from flask import render_template
-#    return render_template('index.html')
-#application = Flask(__name__)
 application = Flask(__name__)
 
-#data = cursor.fetchall()
-#return render_template('db.html', data = data)
 
 @application.route("/")
-#def db():
-#	connection =  MySQLdb.connect (host = "localhost", user = "root", passwd = "!Giamal76", db = "employees")
-# open a database connection
-# be sure to change the host IP address, username, password and database name to match your own
-#connection = MySQLdb.connect (host = "localhost", user = "root", passwd = "!Giamal76", db = "employees")
-# prepare a cursor object using cursor() method
-#	cursor = connection.cursor ()
-# execute the SQL query using execute() method.
-#	cursor.execute ("select * from employees.employees where birth_date='1965-02-01' and gender='M' and hire_date>'1990-01-01' order by last_name;")
-# fetch all of the rows from the query
+# define a db connection
 def db():
     db = MySQLdb.connect(host = "localhost", user = "appuser", passwd = "!Temp123", db = "employees" )
 
     cursor = db.cursor()
 
-    cursor.execute("select  first_name,last_name  from employees.employees where birth_date='1965-02-01' and gender='M' and hire_date>'1990-01-01' order by last_name;")
-    #data = cursor.fetchall()
-    #return render_template('db.html', data = data)
+    cursor.execute("select  last_name,first_name,emp_no  from employees.employees where birth_date='1965-02-01' and gender='M' and hire_date>'1990-01-01' order by last_name;")
 
     rows = []
 
     for row in cursor:
-	rows.append(row)
-        #print(row)
-	#print row[2], row[3]
-
-	#return rows
-	data = cursor.fetchall()    
-	return render_template('db.html', data = data)
-# print the rows
-#for row in data :
-#	print row[2], row[3]
-# close the cursor object
-#cursor.close ()
-# close the connection
-#connection.close ()
-#db.close ()
-# exit the program
-#sys.exit()
+        rows.append(row)
+#return rows
+        data = cursor.fetchall()
+        return render_template('db.html', data = data)
+# run flask
 if __name__ == '__main__':
     application.run()
+
